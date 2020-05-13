2797d968f484ab10d0f814d718416ddbf7cfa163
Ludvig Lindblad <ludvig@lindblad.net>
2020-05-13 19:25:32 +0200

    Night cam functionality

--- stream.sh
+++ stream.sh
@@ -5,30 +5,30 @@ day="0"
 while true
 do
   hour=$(date +\"%H\")
-  if [[ "$hour" =~ ^(21|22|23|00|01|02|03|04|05|06)$ ]]; then
+  if [[ $hour =~ ^(\"21\"|\"22\"|\"23\"|\"00\"|\"01\"|\"02\"|\"03\"|\"04\"|\"05\"|\"06\"|\"19\")$ ]]; then
     if [ $day == "1" ]
     then
       killall ffmpeg
-      steaming="0"
+      streaming="0"
     fi
     day="0"
-    stream_cmd="-f video4linux2 -s 1820x1080 -framerate 24 -i /dev/video1 -c:v h264_omx -threads 0 -an -f flv"
+    stream_cmd="-f video4linux2 -s 1920x1080 -framerate 30 -i /dev/video2 -c:v h264_omx -threads 0 -an -f flv"
   else
     if [ $day == "0" ]
       then
       killall ffmpeg
-      steaming="0"
+      streaming="0"
     fi
     day="1"
     stream_cmd="-f video4linux2 -s 1280x720 -framerate 24 -i /dev/video0 -c:v h264_omx -threads 0 -an -f flv"
   fi
-  ping -c 1 www.twitch.tv &> /dev/null 
+  ping -c 1 www.twitch.tv &> /dev/null
   if [[ $? -eq 0 ]] ; then
     if [ $streaming -eq "0" ]
     then
       echo "Connection established. Starting stream in 60 seconds."
-      sleep 60
-      ../FFMpeg/ffmpeg $stream_cmd $stream_url\"$live_key\" &
+      sleep 2
+      ../FFMpeg/ffmpeg $stream_cmd $stream_url &
       streaming="1"
     fi
   elif [ $streaming -eq "1" ]
